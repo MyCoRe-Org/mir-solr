0e911ada84e912dd0decfbe12f6f5ed6d8e15325
Sebastian Hofmann <sebastian.hofmann@gbv.de>
2022-12-16 16:16:44 +0100

    MIR-1189 improve docker containers

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/bash
 set -e
-echo "Running solr entry script!"
-/opt/docker-solr/scripts/precreate-core  mir /opt/solr/server/solr/configsets/mycore_main
-/opt/docker-solr/scripts/precreate-core  mir-classifications /opt/solr/server/solr/configsets/mycore_classification
-solr-foreground -force
+unset SOLR_USER SOLR_UID SOLR_GROUP SOLR_GID \
+      SOLR_CLOSER_URL SOLR_DIST_URL SOLR_ARCHIVE_URL SOLR_DOWNLOAD_URL SOLR_DOWNLOAD_SERVER SOLR_KEYS SOLR_SHA512
+
+echo "Running solr entry script as user: $(whoami)"
+if [ "$(id -u)" -eq 0 ]; then
+  chown -R solr:solr /var/solr/
+  exec /usr/bin/sudo -E -u solr "PATH=$PATH" "$(pwd)/$0";
+  exit 0;
+fi
+/opt/docker-solr/scripts/precreate-core mir /opt/solr/server/solr/configsets/mycore_main
+/opt/docker-solr/scripts/precreate-core mir-classifications /opt/solr/server/solr/configsets/mycore_classification
+/opt/docker-solr/scripts/solr-foreground;
