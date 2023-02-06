d808cd9682c1e28d9be4a56995b545929cb05d14
Sebastian Hofmann <sebastian.hofmann@gbv.de>
2023-02-06 12:44:47 +0100

    MIR-1196 docker container startup is very slow

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,9 +3,13 @@ set -e
 unset SOLR_USER SOLR_UID SOLR_GROUP SOLR_GID \
       SOLR_CLOSER_URL SOLR_DIST_URL SOLR_ARCHIVE_URL SOLR_DOWNLOAD_URL SOLR_DOWNLOAD_SERVER SOLR_KEYS SOLR_SHA512
 
+function fixDirectoryRights() {
+  find "$1" \! -user "$2" -exec chown "$2:$2" '{}' +
+}
+
 echo "Running solr entry script as user: $(whoami)"
 if [ "$(id -u)" -eq 0 ]; then
-  chown -R solr:solr /var/solr/
+  fixDirectoryRights /var/solr/ solr
   exec /usr/bin/sudo -E -u solr "PATH=$PATH" "$(pwd)/$0";
   exit 0;
 fi
